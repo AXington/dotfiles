54c8ee5d535d49db02e96057c2fe3175bcdcf916
Alice Thomas <2721182+AXington@users.noreply.github.com>
2017-05-17 15:39:28 -0400

    update readme and setup for missed configs

--- setup.sh
+++ setup.sh
@@ -11,10 +11,13 @@ ln -s -f $DIR/vim $HOME/.vim
 ln -s -f $DIR/vimrc $HOME/.vimrc
 ln -s -f $DIR/zshrc $HOME/.zshrc
 
+nocaps="setxkbmap -option ctrl:nocaps\n
+xcape -e 'Caps_Lock=Escape;Control_L=Escape;Control_R=Escape'"
+
 echo "Is current env linux?"
 select yn in "Yes" "No"; do
 	case $yn in
-		Yes ) echo "setxkbmap -option ctrl:nocaps" >> $HOME/.local_settings; break;;
+		Yes ) echo "$nocapS" >> $HOME/.local_settings; break;;
 		No ) echo "not disabling capslock"; break;;
 	esac
 done
