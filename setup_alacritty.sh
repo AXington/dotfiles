1477ae73d50d4146243aafdec580d7968af678aa
Alice Thomas <2721182+AXington@users.noreply.github.com>
2019-07-18 16:06:04 -0400

    refactor to make seup more robust and clear

---
+++ setup_alacritty.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+setup_completions(){
+    if [[ ! -f "${ZDOTDIR:-~}/.zsh_functions/_alacritty" ]]; then
+        mkdir -p ${ZDOTDIR:-~}/.zsh_functions
+        echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
+        curl -o cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
+    fi
+}
+
+
+setup_man_pages(){
+    man_path = "/usr/local/share/man/man1"
+    man_page_file = "${man_path}/alacritty.1.gz"
+    if [[ ! -f "${man_page_file}" ]]; then
+        sudo mkdir -p ${man_path}
+        curl -O https://raw.githubusercontent.com/jwilm/alacritty/master/extra/alacritty.man
+        gzip -c alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+        rm alacritty.man
+    fi
+}
+
+setup_terminfo(){
+    curl -O https://raw.githubusercontent.com/jwilm/alacritty/master/extra/alacritty.info
+    sudo tic -xe alacritty,alacritty-direct alacritty.info
+    rm alacritty.info
+}
+
+install_mac(){
+    if [[ ! hash brew 2>/dev/null ]]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+
+    brew update && brew cask install alacritty
+
+    if [[ ! hash gzip 2>/dev/null ]]; then
+        brew install gzip
+    fi
+}
+
+install_linux(){
+    if [[ hash apt-get ]];then
+        sudo add-apt-repository ppa:mmstick76/alacritty
+        sudo apt-get update && sudo apt-get install -y gzip alacritty
+    elif [[ hash pacman ]]; then
+        pacman -S alacritty gzip
+    else
+        echo "Only deb/ubuntu and arch based distros are supported"
+        exit 1
+    fi
+}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    install_mac
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    install_linux
+fi
+
+setup_man_pages
+setup_completions
+setup_terminfo
+
+mkdir -p $HOME/.config/alacritty
+sudo ln -s -f $(pwd)/terminal/configs/alacritty.yml $HOME/.config/alacritty/.
+
