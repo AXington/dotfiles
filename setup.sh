698f2a3d868cbbedd8ce372d898e4948baedc082
Alice Thomas <alice.thomas@waystar.com>
2026-01-20 15:19:26 -0500

    update brew packages

--- setup.sh
+++ setup.sh
@@ -76,16 +76,16 @@ bind n next-window
 EOF
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 
 # Set up zshrc options
-sed -i.bak -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/'
+#sed -i.bak -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/'
 
 # add mac specific options and gnu bins to path
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    cat << 'EOF' >> ~/.zshrc
-export HOMEBREW_CELLAR="/usr/local/Cellar"
-EOF
+#    cat << 'EOF' >> ~/.zshrc
+#export HOMEBREW_CELLAR="/usr/local/Cellar"
+#EOF
     # make all the gnu binaries default
     echo "export PATH=$HOME/.gnubin:$PATH" >> ~/.zshrc
 fi
@@ -95,32 +95,32 @@ if  hash update-alternatives 2>/dev/null; then
     sudo update-alternatives --set editor /usr/bin/vim.basic
 fi
 
-cat << 'EOF' >> ~/.zshrc
-export LANG=en_US.UTF-8
-#alias nuke_docker='docker rm --force $(docker ps -a -q)'
-autoload -U +X bashcompinit && bashcompinit
-export EDITOR='vim'
-export SSH_KEY_PATH="~/.ssh/rsa_id"
-prompt_context(){}
-export GPG_TTY=`tty`
-bindkey '^R' history-incremental-search-backward
+#cat << 'EOF' >> ~/.zshrc
+#export LANG=en_US.UTF-8
+##alias nuke_docker='docker rm --force $(docker ps -a -q)'
+#autoload -U +X bashcompinit && bashcompinit
+#export EDITOR='vim'
+#export SSH_KEY_PATH="~/.ssh/rsa_id"
+#prompt_context(){}
+#export GPG_TTY=`tty`
+#bindkey '^R' history-incremental-search-backward
 #source <(helm completion zsh)
 #source <(kubectl completion zsh)
 #source /usr/local/etc/bash_completion.d/az
-fpath+=${ZDOTDIR:-~}/.zsh_functions
-tmux attach || tmux new
-EOF
+#fpath+=${ZDOTDIR:-~}/.zsh_functions
+#tmux attach || tmux new
+#EOF
 
 # add zsh-syntax-highlighting plugin to omz plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # add default plugins to .zshrc
-for PLUGIN in ['zsh-syntax-highlighting']; do
-    sed '/plugins=(/a\'$'\n' ${PLUGIN} ~/.zshrc
-done
+#for PLUGIN in ['zsh-syntax-highlighting']; do
+#    sed '/plugins=(/a\'$'\n' ${PLUGIN} ~/.zshrc
+#done
 
-ln -s -f scripts/clean_docker_cache /usr/local/bin/.
-ln -s -f scripts/clean_python_cache /usr/local/bin/.
+#ln -s -f scripts/clean_docker_cache /usr/local/bin/.
+#ln -s -f scripts/clean_python_cache /usr/local/bin/.
 
 git clone https://github.com/AXington/.vim.git $HOME/.vim
 current_dir=$(pwd)
@@ -129,5 +129,5 @@ ln -s -f $HOME/.vim/.vimrc $HOME/.vimrc
 ln -s -f $HOME/.vim/.vimrc.local $HOME/.vimrc.local
 
 # set zsh as default shell and start zsh
-sudo chsh $USER -s $(which zsh)
-exec zsh -l
+#sudo chsh $USER -s $(which zsh)
+#exec zsh -l
